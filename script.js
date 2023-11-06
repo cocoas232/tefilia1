130cf7c28bc72ce77327eb8130f0b524992b3917
cocoas232 <113993316+cocoas232@users.noreply.github.com>
2023-11-06 22:36:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const promo = document.querySelector('#promo')
+const button = document.querySelector('button')
+
+button.addEventListener('click', function () {
+    promo.classList.toggle('promik')
+});
